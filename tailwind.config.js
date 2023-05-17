555f37490c16c63a08d512544c34566eb7681c13
Krish Singal <krish.singal2002@gmail.com>
2023-05-17 17:37:46 -0500

    Website iteration one

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./public/miscellaneous.html", "./resume.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
